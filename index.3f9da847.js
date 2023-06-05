cd1dc7b429787b59a647bd15324632cac675b4e9
Nickisilver <119536445+Nickisilver@users.noreply.github.com>
2023-06-05 19:52:26 +0000

    Deploying to gh-pages from @ Nickisilver/goit-js-hw-11@05f51dccbd44dac13c012fae0d6daef1ad89d6a1 🚀

--- index.3f9da847.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('[name="searchQuery"]');
-//# sourceMappingURL=index.3f9da847.js.map
--- index.c95823db.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('[name="searchQuery"]');
-//# sourceMappingURL=index.c95823db.js.map
